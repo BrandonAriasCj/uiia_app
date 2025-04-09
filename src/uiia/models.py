5494aa5b8a20244503053e44f418949e1ba6c8a6
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-04-09 14:40:41 -0500

    Definicion de los modelos

--- src/uiia/models.py
+++ src/uiia/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Componente(models.Model):
+    nombre = models.CharField(max_length=100)
+#    historial_chat = models.JSONField(default=list)
+    descripcion = models.TextField(blank=True)
+    prompt = models.TextField(blank=True)
+    codigo_generado = models.TextField(blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
